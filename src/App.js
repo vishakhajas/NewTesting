dcc514eac3ceea967493c45fd0b8b858d7240baf
vishakhajas <vishakha17july@gmail.com>
2025-03-18 10:39:38 +0100

    FeedBlog branch

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-// import Feedblog from "./FeedBlog/Feedblog.tsx";
-import Testing from "./Tester/Testing.tsx";
+import Feedblog from "./FeedBlog/Feedblog.tsx";
+// import Testing from "./Tester/Testing.tsx";
 
 
 function App() {
     return (
         <Router>
             <Routes>
-                <Route path="/" element={<Testing />} />
-                {/* <Route path='/' element={<Feedblog />} /> */}
+                {/* <Route path="/" element={<Testing />} /> */}
+                <Route path='/' element={<Feedblog />} />
             </Routes>
         </Router>
     );
